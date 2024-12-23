2143a5fa45a31b1ed35d9a5811358d657d805b58
jleblanc64 <dabadich@gmail.com>
2024-12-23 18:33:37 +0100

    fill package custom

---
+++ src/main/java/io/github/jleblanc64/libcustom/custom/jackson/VavrListConverter.java
@@ -0,0 +1,22 @@
+package io.github.jleblanc64.libcustom.custom.jackson;
+
+import com.fasterxml.jackson.databind.util.StdConverter;
+import io.vavr.collection.List;
+
+import java.util.Collection;
+
+public class VavrListConverter {
+    public static class FromCollec extends StdConverter<Collection<?>, List<?>> {
+        @Override
+        public List<?> convert(Collection value) {
+            return List.ofAll(value);
+        }
+    }
+
+    public static class ToCollec extends StdConverter<List<?>, Collection<?>> {
+        @Override
+        public Collection<?> convert(List value) {
+            return value.toJavaList();
+        }
+    }
+}
