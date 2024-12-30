2b5d92abf6a7733b3c807969de8bbe573846e0f3
jleblanc64 <dabadich@gmail.com>
2024-12-30 23:11:09 +0100

    cleanup

--- src/test/java/io/github/jleblanc64/libcustom/retry/TestRetry.java
+++ src/test/java/io/github/jleblanc64/libcustom/retry/TestRetry.java
@@ -2,8 +2,6 @@ package io.github.jleblanc64.libcustom.retry;
 
 import io.github.jleblanc64.libcustom.custom.test.TestRetryDefault;
 
-import static io.github.jleblanc64.libcustom.functional.Functor.print;
-
 public class TestRetry extends TestRetryDefault {
     static {
         MAX_ATTEMPTS = 5;
@@ -13,11 +11,6 @@ public class TestRetry extends TestRetryDefault {
 
     static boolean isIntelliJ() {
         var javaCmd = System.getProperty("sun.java.command", "").toLowerCase();
-
-        var isIntelliJ = javaCmd.contains("intellij");
-        if (isIntelliJ)
-            print("Tests run from IntelliJ");
-
-        return isIntelliJ;
+        return javaCmd.contains("intellij");
     }
 }
--- src/test/java/io/github/jleblanc64/libcustom/retry/TestRetryTests.java
+++ src/test/java/io/github/jleblanc64/libcustom/retry/TestRetryTests.java
@@ -2,7 +2,6 @@ package io.github.jleblanc64.libcustom.retry;
 
 import org.junit.jupiter.api.Test;
 
-import static io.github.jleblanc64.libcustom.functional.Functor.print;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestRetryTests extends TestRetry {
@@ -11,8 +10,6 @@ public class TestRetryTests extends TestRetry {
         assertEquals(1, 1);
 
 //        assertEquals(1, 2);
-
-        print("success");
     }
 }
 
