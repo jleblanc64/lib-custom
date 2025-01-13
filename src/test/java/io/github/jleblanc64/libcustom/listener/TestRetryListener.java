cda6c6397021a1c44f3e29afc2d40eee14985274
jleblanc64 <dabadich@gmail.com>
2025-01-13 17:00:16 +0100

    cleanup

--- src/test/java/io/github/jleblanc64/libcustom/listener/TestRetryListener.java
+++ src/test/java/io/github/jleblanc64/libcustom/listener/TestRetryListener.java
@@ -3,6 +3,7 @@ package io.github.jleblanc64.libcustom.listener;
 import io.github.jleblanc64.libcustom.custom.test.TestRetry;
 import org.junit.platform.launcher.TestExecutionListener;
 
+// setup in src/test/resources/META-INF/services/org.junit.platform.launcher.TestExecutionListener
 public class TestRetryListener extends TestRetry implements TestExecutionListener {
     public TestRetryListener() {
         MAX_ATTEMPTS = 5;
