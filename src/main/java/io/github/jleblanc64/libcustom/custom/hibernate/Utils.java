b7aab7dbfe6cb12a0e8bf23033a8de1a53b92ca1
jleblanc64 <dabadich@gmail.com>
2025-02-19 01:31:09 +0100

    cleanup

--- src/main/java/io/github/jleblanc64/libcustom/custom/hibernate/Utils.java
+++ src/main/java/io/github/jleblanc64/libcustom/custom/hibernate/Utils.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2024 - Charles Dabadie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.jleblanc64.libcustom.custom.hibernate;
 
-import io.github.jleblanc64.libcustom.custom.hibernate.duplicate.MyPersistentBag;
 import io.github.jleblanc64.libcustom.functional.ListF;
 import io.github.jleblanc64.libcustom.meta.WithClass;
 import lombok.SneakyThrows;
+import org.hibernate.collection.internal.AbstractPersistentCollection;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
@@ -14,7 +29,7 @@ import static io.github.jleblanc64.libcustom.Reflection.getAllFields;
 import static io.github.jleblanc64.libcustom.functional.ListF.f;
 
 public class Utils {
-    private static Class PERSISTENT_COLLECTION_CLASS = MyPersistentBag.class;
+    private static Class PERSISTENT_COLLECTION_CLASS = AbstractPersistentCollection.class;
 
     @SneakyThrows
     private static Field roleToField(String role) {
--- src/main/java/io/github/jleblanc64/libcustom/custom/hibernate/duplicate/MyPersistentBag.java
+++ src/main/java/io/github/jleblanc64/libcustom/custom/hibernate/duplicate/MyPersistentBag.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 - Charles Dabadie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.jleblanc64.libcustom.custom.hibernate.duplicate;
 
 import org.hibernate.HibernateException;
@@ -13,14 +28,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
-/**
- * An unordered, unkeyed collection that can contain the same element
- * multiple times. The Java collections API, curiously, has no <tt>Bag</tt>.
- * Most developers seem to use <tt>List</tt>s to represent bag semantics,
- * so Hibernate follows this practice.
- *
- * @author Gavin King
- */
 public class MyPersistentBag extends AbstractPersistentCollection {
 
     protected List bag;
